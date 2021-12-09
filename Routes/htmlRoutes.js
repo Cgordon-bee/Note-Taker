f271cb26e7bb336be36c9600badab6f0cb1f5654
Cgordon-bee <81937022+Cgordon-bee@users.noreply.github.com>
2021-12-09 21:57:45 +0000

    amended new routes

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const router = require('express').Router();
 
+
+
 //Sends notes to the notes.html file
 router.get('api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
 
-//Sends to the homepage if a pathing issue exists
+
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
--- server.js
+++ server.js
@@ -1,47 +1,61 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const util = require('util');
 const apiRoutes = require("./Routes/apiRoutes");
 
-
-// Set up for the express server & End points
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-
-//The public folder holds our base files i.e css, html etc, these are known as static files and are not likely to change
-//app.use is our firststep in connecting to a "middleware" - middleware is another layer in which our requests are routed
-
-// Built-in express method to call the next middleware in the stack.
-// next();
+// middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// app.use(express.static("db"));
+app.use(express.static("db"));
 app.use(express.static('public'));
 
 app.use('/api/notes', apiRoutes);
 
 
-
 //this section will set up 3 main routes, get, post & delete
 
 
-// app.get is a method to route to homepage  shortcode =('/')
-app.get('api/notes', (req, res) => 
+// app.get is a method to route to notes.html in the public folder.
+app.get('api/notes', (req, res) => {
 res.sendFile(path.join(__dirname,"./public/notes.html"))
-);
+});
 
 app.get("*", (req, res) =>{
-
-  res.sendFile(path.join(__dirname, "./public/send.html"))
+res.sendFile(path.join(__dirname, "./public/index.html"))
 });
+app.post('/api/notes', async(req, res) => {
+  const db = await readFileAsync('db/db.json', 'utf8')
+console.log(db)
+// fs.readFile(`${__dirname}./db/db.json`, "utf8", function (err){
+//   if (!err) {
+//           console.log(err);
+    
+//         } 
+//           savedNotes =JSON.parse(savedNotes);
+    
+//           const viewNewNote ={
+//             title: req.body.title,
+//             text: req.body.text,
+//             id:savedNotes.length + 1,
+    
+          
+//         }
+    
+
+// })
+
+})
 
 
 //app.listen is a method that accepts two arguments, 1st argument is to invoke our server, the 2nd argument is a callback function.
-app.listen(PORT, () =>
+app.listen(PORT, () =>{
   console.log(`server connected successfully http://localhost:${PORT}`)
-);
-
-
-
+});
